e3d8fea2b61851d4e54d2bae01be34339af8a592
XxAbrahamLincolnxX <asajernigan1894@gmail.com>
2025-06-08 12:58:32 -0700

    updated backgrounds header global things

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function toggleMenu() {
     toggleButton.setAttribute('aria-expanded', String(!isExpanded));
   }
   
+
+  window.addEventListener('scroll', () => {
+    const header = document.querySelector('.site-header');
+    if (window.scrollY > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
